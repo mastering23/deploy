91ddc16e4aa73db5fc71057a48206c16556cb047
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-02-17 17:02:27 -0500

    combines server + react

--- server.cjs
+++ server.cjs
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const employees = require("./database");
+const employees = require("./database.cjs");
 
-app.get("/", (req, res) => res.json("Hello employees!"));
+app.use(express.static('dist'))
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/dist/index.html`);
+});
 
 app.get("/employees", (req, res) => res.json(employees));
 
